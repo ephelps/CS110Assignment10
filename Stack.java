b74c8155d344560e2ce75ae7e804e98a10959c14
Emily Phelps <ephelps@uvm.edu>
2014-04-25 18:16:35 -0400

    Stack class
    splits the deck in two for war game

---
+++ Stack.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Stack
+{  
+   //stacks for each of the players
+   ArrayList<Card> stackPlayer1;
+   ArrayList<Card> stackPlayer2;
+
+   /**
+      constructor will take in a deck, split the deck in two and assign each half 
+      of the deck to each players array
+      @param d a Deck object
+   */
+   public Stack(Deck d)
+   {
+   
+   }
+}
