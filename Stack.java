419d5856d68eaf723dc9e7ebf3a31a26f5b1b33f
Emily Phelps <ephelps@uvm.edu>
2014-04-25 19:04:33 -0400

    added methods
    created methods for dealing card, returning number of cards in stack and
    adding a card to a stack for both players stacks

--- Stack.java
+++ Stack.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class Stack
+public class Stack extends Deck
 {  
    //stacks for each of the players
    ArrayList<Card> stackPlayer1;
@@ -14,5 +14,63 @@ public class Stack
    public Stack(Deck d)
    {
    
+   }
+   
+   /**
+      dealCardPlayer1 will return a card from player 1's stack
+      @return a card objects
+   */
+   public Card dealCardPlayer1()
+   {
+      Card player1 = stackPlayer1.remove(0);
+      
+      return new Card(player1);
+   }
+   
+   /**
+      dealCardPlayer2 will return a card from player 2's stack
+      @return a card objects
+   */
+   public Card dealCardPlayer2()
+   {
+      Card player2 = stackPlayer2.remove(0);
+      
+      return new Card(player2);
+   }
+   
+   /**
+      cardsRemainingPlayer1 will return the number of cards in the players stack
+      @return an int of the number of cards
+   */
+   public int cardsRemainingPlayer1()
+   {
+      return stackPlayer1.size();
+   }
+   
+   /**
+      cardsRemainingPlayer2 will return the number of cards in the players stack
+      @return an int of the number of cards
+   */
+   public int cardsRemainingPlayer2()
+   {
+      return stackPlayer2.size();
+   }
+   
+   /**
+      addCardPlayer1 will add a card object to a player 1's stack
+      @param a card object to be added
+   */
+   public void addCardPlayer1(Card c)
+   {
+      stackPlayer1.add(c);
+   }
+   
+   /**
+      addCardPlayer2 will add a card object to a player 2's stack
+      @param a card object to be added
+   */
+   public void addCardPlayer2(Card c)
+   {
+      stackPlayer2.add(c);
    }
 }
