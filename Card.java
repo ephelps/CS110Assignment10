ec70e6ce33f0d0def57055bde21d640a735aeb8e
Emily Phelps <ephelps@uvm.edu>
2014-04-25 17:31:22 -0400

    Card Class
    the card class will create a card item that contains a rank, a suite,
    and a pic (when i get it to work)

---
+++ Card.java
@@ -0,0 +1,90 @@
+//Emily Phelps
+//CS 110
+//Card Class
+
+/*represents a single playing card in a deck of 52
+a card object is created with a rank, a suite, and a pic and cannot be mondified
+two cards are equal if they contain the same rank
+*/
+
+import javax.swing.*;
+
+public class Card
+{
+   //create enumerated data type for suite
+   enum suite {HEARTS, CLUBS, DIAMONDS, SPADES};
+   
+   //define enumerated rank constants
+   enum rank {JACK, QUEEN, KING, ACE};
+   
+   //define fields
+   private int rank;
+   private int suite;
+   private ImageIcon pic;
+   
+   /**
+      constructor will accept a rank and suite for a card and set them to rank and suite respectively
+      will also set a pic to the card accordingly
+      @param cardRank the rank of the card
+      @param cardSuite the suite of the card
+   */
+   
+   public Card(int suite, int rank)
+   {
+      this.rank = rank;
+      this.suite = suite;
+      pic = null;////////////////////////////////////////////////////////////////////////////////////////////////////
+   }
+   
+   /** 
+      getSuite will return an int of the suite
+      @return the int # of the suite
+   */
+   
+   public int getSuite()
+   {
+      return suite;
+   }
+   
+   /**
+      getRank will return an int of the rank
+      @return the int # of the rank of the card
+   */
+   
+   public int getRank()
+   {
+      return rank;
+   }
+   
+   /**
+      getPic will return the pic attached to the card
+      @return a ImageIcon pic of a card
+   */
+   public ImageIcon getPic()
+   {
+      return pic;
+   }
+   
+   /**
+      equals will accept a Card object, compare, and if they are equal return a boolean true
+      @param card2 a Card object to be compared
+      @return boolean true or false
+   */
+   
+   public boolean equals(Card card2)
+   {
+      boolean status;
+      
+      if (rank == card2.rank)
+      {
+         status = true;
+      }
+      
+      else
+      {
+         status = false;
+      }
+      
+      return status;
+   }
+}
