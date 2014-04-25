21c6e3af681947b3b5daa3ecd366f9d3c0a9fd5c
Emily Phelps <ephelps@uvm.edu>
2014-04-25 18:16:19 -0400

    added shuffle back in, realized I would need it in program

--- Deck.java
+++ Deck.java
@@ -59,5 +59,22 @@ public class Deck
    {
       return (deck.size() == 0);
    }
+   
+   /**
+      shuffle randomizes the deck
+   */
+   public void shuffle()
+   {
+      int randNum;
+      Card temp;
+      Random r = new Random();
+      for (int i = 0; i < deck.size(); i++)
+      {
+         randNum = r.nextInt(deck.size());
+         temp = deck.get(i);
+         deck.set(i,deck.get(randNum));
+         deck.set(randNum,temp);
+      }      
+   }
 }
 
