cff7d7831178009715e509fdad25b7566c43f069
Emily Phelps <ephelps@uvm.edu>
2014-04-29 10:10:32 -0400

    changed loser class

--- WarInterface.java
+++ WarInterface.java
@@ -3,5 +3,5 @@ public interface WarInterface
    public void play();
    public int roundWinner();
    public void war(Card c1, Card c2);
-   public void loser(int s1, int s2);
+   public void loser();
 }
