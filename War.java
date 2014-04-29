4e989399772b5bbc665aa876cac8cd60e4b42580
Emily Phelps <ephelps@uvm.edu>
2014-04-29 10:11:40 -0400

    changes
    war class was changed to work for multiple wars, then was tested- it
    works, got rid of command line prompts

--- War.java
+++ War.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class War
 {  
@@ -5,6 +6,7 @@ public class War
    private Stack s;
    private int cardsPlayer1;
    private int cardsPlayer2;
+   private ArrayList<Card> warCards;
    
    /**
       constructor creates a new stack object and sets
@@ -26,44 +28,32 @@ public class War
    {
       Card c1 = s.dealCardPlayer1();
       Card c2 = s.dealCardPlayer2();
-      
-      System.out.println(c1.toString());
-      System.out.println(c2.toString());
+            
       int theWinner = roundWinner(c1,c2);  
       
       if(theWinner == -1)//if player 2 won, player 1's card should be added to their pile
       {
-         System.out.println(c1.toString() + " was less than " + c2.toString());
          s.addCardPlayer2(c1);
          s.addCardPlayer2(c2);
       }
       
       if(theWinner == 1)//if player 1 won, player 2's card should be added to their pile
       {
-         System.out.println(c2.toString() + " was less than " + c1.toString());
          s.addCardPlayer1(c2);
          s.addCardPlayer1(c1);
       }  
       
       if(theWinner == 0)
       {
-         System.out.println("The cards were equal- war will commence");
          war(c1,c2);
-      }
-      
-      if(theWinner == 1111)
-      {
-         System.out.println("An error occurred");
       }
       
       cardsPlayer1 = s.cardsRemainingPlayer1();
       cardsPlayer2 = s.cardsRemainingPlayer2();
-      
-      System.out.println("Player 1 has " + cardsPlayer1 + " cards remaining and Player 2 has " + cardsPlayer2 + " cards remaining");
-      
+            
       if (cardsPlayer1 == 0 || cardsPlayer2==0)
       {
-         loser(cardsPlayer1, cardsPlayer2);
+         loser();
       }
    }
    
@@ -77,7 +67,7 @@ public class War
    */
    public int roundWinner(Card c1, Card c2)
    {
-      int winnerNumber = 1111;
+      int winnerNumber = 999;
       
       if(c1.equals(c2))
       {
@@ -103,88 +93,79 @@ public class War
       @param c1 player 1's equal card
       @param c2 player 2's equal card
    */
+   
+   //USE ARRAYS AND WHILE LOOP add all cards at the end of the loop- when war winner !=0
    public void war(Card c1, Card c2)
    {  
+      int warWinner = 0;
+      
+      warCards = new ArrayList<Card>();
+      
+      warCards.add(c1);
+      warCards.add(c2);
+      
+      while (warWinner == 0)
+      {    
          //draw one card from each person
          Card c1FaceDown = s.dealCardPlayer1();
          Card c2FaceDown = s.dealCardPlayer2();
-                     
+         
+         //add cards to array
+         warCards.add(c1FaceDown);
+         warCards.add(c2FaceDown);
+         
+         //check to see that there are not 0 cards left in either persons deck
+         loser();
+         
+         //draw the second card on each pile            
          Card c1FaceUp = s.dealCardPlayer1();
          Card c2FaceUp = s.dealCardPlayer2();
-            
-         System.out.println(c1FaceUp.toString());
-         System.out.println(c2FaceUp.toString());
-            
+         
+         //add cards to array
+         warCards.add(c1FaceUp);
+         warCards.add(c2FaceUp);
+                                 
          //send the face up cards to roundWinner
-         int warWinner = roundWinner(c1FaceUp, c2FaceUp);
-            
-         if(warWinner == -1)//if player 2 won, player 1's cards should be added to their pile
-         {
-            s.addCardPlayer2(c1);
-            s.addCardPlayer2(c1FaceDown);
-            s.addCardPlayer2(c1FaceUp);
-            s.addCardPlayer2(c2);
-            s.addCardPlayer2(c2FaceDown);
-            s.addCardPlayer2(c1FaceUp);
-
-            System.out.println(c1FaceUp.toString() + " was less than " + c2FaceUp.toString());
-            System.out.println("The cards "+ c1.toString() + ", " + c1FaceDown.toString() + 
-                                 " and " + c1FaceUp.toString() + " were added to Player 2's stack");
-         }
+         warWinner = roundWinner(c1FaceUp, c2FaceUp);
          
-         if(warWinner == 1)//if player 1 won, player 2's cards should be added to their pile
-         {
-            s.addCardPlayer1(c2);
-            s.addCardPlayer1(c2FaceDown);
-            s.addCardPlayer1(c2FaceUp);
-            s.addCardPlayer1(c1);
-            s.addCardPlayer1(c1FaceDown);
-            s.addCardPlayer1(c1FaceUp);
-            System.out.println(c2FaceUp.toString() + " was less than " + c1FaceUp.toString());
-            System.out.println("The cards "+ c2.toString() + ", " + c2FaceDown.toString() + 
-                                 " and " + c2FaceUp.toString() + " were added to Player 1's stack");
-
-         }
-            
-         cardsPlayer1 = s.cardsRemainingPlayer1();
-         cardsPlayer2 = s.cardsRemainingPlayer2();
-
-         if(warWinner == 1111)
-         {
-            System.out.println("An error occurred");
-         }
+      }
          
-         if(cardsPlayer1 <= 1 || cardsPlayer2 <= 1)
+      if(warWinner == -1)//if player 2 won, player 1's cards should be added to their pile
+      {
+         for(int i = 0; i <= warCards.size(); i++)
          {
-            cardsPlayer1 = s.cardsRemainingPlayer1();
-            cardsPlayer2 = s.cardsRemainingPlayer2();
-            System.out.println("Not enough cards to continue war");
-            loser(cardsPlayer1, cardsPlayer2);
+            Card c = warCards.get(0);
+            s.addCardPlayer2(c);
+         }               
+      }
+            
+      if(warWinner == 1)//if player 1 won, player 2's cards should be added to their pile
+      {
+         for(int i = 0; i <= warCards.size(); i++)
+         {
+            Card c = warCards.get(0);
+            s.addCardPlayer2(c);
          }
-      
+      }
    }  
    
    /**
-         loser determines who lost the game
-         @param s1 the number of cards in player 1's stack
-         @param s2 the number of cards in player 2's stack
+         loser determines if someone lost the game
    */
    
-   public void loser(int s1, int s2)
+   public void loser()
    {
-      if (s1 == 0)
-      {
-         System.out.println("Player 1 lost");
-      }
-      
-      if (s2 == 0)
-      {
-         System.out.println("Player 2 lost");
-      }
-      
-      else
+      cardsPlayer1 = s.cardsRemainingPlayer1();
+      cardsPlayer2 = s.cardsRemainingPlayer2();  
+
+      if (cardsPlayer1 == 0)
       {
       
       }
+      
+      if (cardsPlayer2 == 0)
+      {
+      }
+
    }
 }
