616f2eb6995f2735bccec6f8470b728655f5861f
Emily Phelps <ephelps@uvm.edu>
2014-04-29 23:19:47 -0400

    added new stuff to interface

--- WarInterface.java
+++ WarInterface.java
@@ -2,9 +2,10 @@ import java.util.ArrayList;
 
 public interface WarInterface
 {
-   public ArrayList<Card> play();
+   public ArrayList<Card> playRound();
    public int roundWinner(ArrayList<Card> warCards);
-   public void war(ArrayList<Card> warCards);
+   public ArrayList<Card> war(ArrayList<Card> warCards);
+   public int warWinner(ArrayList<Card> warCards);
    public void loser();
    public Card getC1();
    public Card getC2();
