2e3c6f645515e8513602d844cb53fb35220d1dfc
Emily Phelps <ephelps@uvm.edu>
2014-04-29 20:49:53 -0400

    Updated Interfaces

--- CardInterface.java
+++ CardInterface.java
@@ -5,7 +5,5 @@ public interface CardInterface
    public Suite getSuite();
    public int getRank();
    public ImageIcon getPic();
-   public boolean equals(Card card2);
-   public String toString();
-   
+   public boolean equals(Card card2);   
 }
--- WarInterface.java
+++ WarInterface.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+
 public interface WarInterface
 {
-   public void play();
-   public int roundWinner();
-   public void war(Card c1, Card c2);
+   public ArrayList<Card> play();
+   public int roundWinner(ArrayList<Card> warCards);
+   public void war(ArrayList<Card> warCards);
    public void loser();
+   public Card getC1();
+   public Card getC2();
+   public int getCardsRemainingPlayer1();
+   public int getCardsRemainingPlayer2();
 }
